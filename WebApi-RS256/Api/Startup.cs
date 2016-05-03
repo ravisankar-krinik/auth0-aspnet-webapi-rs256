179f9941d72905c849e9527ac01dfe6356063e2c
Jerrie Pelser <jerrie@jerriepelser.com>
2016-05-03 16:54:45 +0700

    Initial commit

---
+++ WebApi-RS256/Api/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Web.Configuration;
+using System.Web.Http;
+
+using Api;
+
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.ActiveDirectory;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var issuer = WebConfigurationManager.AppSettings["Auth0Domain"];
+            var audience = WebConfigurationManager.AppSettings["Auth0ClientID"];
+
+            app.UseCors(CorsOptions.AllowAll);
+
+            app.UseActiveDirectoryFederationServicesBearerAuthentication(
+                new ActiveDirectoryFederationServicesBearerAuthenticationOptions
+                {
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = audience,
+                        ValidIssuer = issuer,
+                        IssuerSigningKeyResolver = (token, securityToken, identifier, parameters) => parameters.IssuerSigningTokens.FirstOrDefault().SecurityKeys.FirstOrDefault()
+                    },
+                    // Setting the MetadataEndpoint so the middleware can download the RS256 certificate
+                    MetadataEndpoint = string.Format("{0}/wsfed/{1}/FederationMetadata/2007-06/FederationMetadata.xml", issuer.TrimEnd('/'), audience)
+                });
+
+            var configuration = new HttpConfiguration();
+            configuration.MapHttpAttributeRoutes();
+            configuration.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+            app.UseWebApi(configuration);
+        }
+    }
+}
